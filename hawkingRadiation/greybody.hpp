9e3487a3ce5650c86ba10d1b38e281c138035fee
QED137 <janmajay@iitdalumni.com>
2025-12-16 12:56:14 +0100

    Remove wrong-folder from repository and add to .gitignore

--- hawkingRadiation/greybody.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "teukolsky_radial.hpp"
-
-double computeGreybody(const TeukolskySolution& sol, const Params& P);
-
--- hawkingRadiation/hawking.hpp
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-#include "greybody.hpp"
-
-double hawkingSpectrum(const Params& P, double Gamma);
-double hawkingTemperature(double M, double a);
-
